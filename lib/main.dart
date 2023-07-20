0e5895db0f35bfa4af2efb1f2dd99cd5fae6f0ec
Arthur <arthurbufon2052@gmail.com>
2023-07-20 19:55:29 -0300

    theming adjusts

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_expense_tracker/widgets/expenses.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(useMaterial3: true, scaffoldBackgroundColor: Color.fromARGB(255, 215, 180, 233)),
       home: const Expenses(),
     ),
   );
