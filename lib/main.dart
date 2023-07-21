589bdad416fbb39eadce1d36dd631b42699cfc7d
Arthur <arthurbufon2052@gmail.com>
2023-07-21 19:43:06 -0300

    colorscheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith(useMaterial3: true, scaffoldBackgroundColor: Color.fromARGB(255, 215, 180, 233)),
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer
+          )
+          ),
       home: const Expenses(),
     ),
   );
