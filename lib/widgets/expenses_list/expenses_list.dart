00bb038de25c9e4ad5373894fc121162b8eb96a2
Arthur <arthurbufon2052@gmail.com>
2023-07-31 18:55:30 -0300

    theme data in widgets

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -18,7 +18,11 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
-        onDismissed: (direction){
+        background: Container(
+          color: Theme.of(context).colorScheme.error,
+          margin: EdgeInsets.symmetric(horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+        ),
+        onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
         },
         child: ExpenseItem(expenses[index]),
